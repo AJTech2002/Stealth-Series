7d4de2476ca5821f77ee2200c3eb7b7a08e5c00d
Ajay Venkat <bestbatchprogrammer101@gmail.com>
2018-01-04 10:20:14 +1000

    Initial commit

---
+++ Parts/Stealth Game Part 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform target;
+	public float distFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2(-40,85);
+
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	private void Start() {
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	private void LateUpdate() {
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		Vector3 e = transform.eulerAngles;
+		e.x = 0;
+
+		target.eulerAngles = e;
+		transform.position = target.position - transform.forward * distFromTarget;
+
+
+
+	}
+
+}
