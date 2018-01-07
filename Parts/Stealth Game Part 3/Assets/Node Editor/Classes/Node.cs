749032601c2c46f268b97f037fc6fa6c93ee9ee9
Ajay Venkat <bestbatchprogrammer101@gmail.com>
2018-01-08 08:37:00 +1000

    Part 3 just got uploaded

---
+++ Parts/Stealth Game Part 3/Assets/Node Editor/Classes/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Node {
+
+	public Rect pos = new Rect (new Vector2 (100, 100), new Vector2 (100, 100));
+	public string name;
+
+	public void DrawNode () {
+		DrawBox ();
+	}
+
+	private void DrawBox() {
+		GUI.Box (pos, name);
+	}
+
+
+	public Node (string _name) {
+		name = _name;
+	}
+
+}
