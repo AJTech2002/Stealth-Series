00417a72a5928bd4bedd85977601032e23d477d6
AJTech2002 <bestbatchprogrammer101@gmail.com>
2018-04-20 16:08:24 +1000

    Part 8 Released
    In this part we created an over the shoulder aiming system...

---
+++ Parts/Stealth Game Part 8/Assets/Scripts/AIMING/ShootingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+    public Animator aimController;
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            aimController.SetInteger("Firing", 1);
+        }
+        else
+        {
+            aimController.SetInteger("Firing", 0);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            aimController.SetInteger("State", 1);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            aimController.SetInteger("State", 0);
+        }
+
+
+    }
+
+
+}
---
+++ Parts/Stealth Game Part 8/Assets/Scripts/IK/RootIK.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootIK : MonoBehaviour {
+
+    public ArmIK arm1;
+    public ArmIK arm2;
+
+    public SphericalConstraint constraint;
+
+    private void LateUpdate()
+    {
+        SolveIK();
+    }
+
+    private void SolveIK()
+    {
+       
+
+        Vector3 arm1V = arm1.SolveBackward(arm1.endEffector.position);
+        Vector3 arm2V = arm2.SolveBackward(arm2.endEffector.position);
+
+        Vector3 centroid = (arm1V + arm2V) / 2;
+
+        centroid = constraint.clampIfNeeded(centroid);
+
+        arm1.SolveForward(centroid);
+        arm2.SolveForward(centroid);
+
+        arm1.SetTempPos();
+        arm2.SetTempPos();
+
+        transform.position = Vector3.Lerp(transform.position,centroid,0.2f);
+
+    }
+
+}
